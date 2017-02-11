7097423a51a0e2542dbeb6ba14c1c608810c820d
vimalaau <vimya7@gmail.com>
2017-02-11 14:19:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,32 +5,96 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+var articleOne = {
+    title: 'Article One | Vimala',
+    heading: 'Article One',
+    date: 'Feb 10 2017',
+    content: ` 
+           <p>
+                 Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.
+           </p>  
+       
+           <p>
+                 Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.
+           </p>
+        
+           <p>
+                 Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.Im just learning how to make websites. Its going to be fabulous.
+                 
+            </p>
+           `
+    };
+    
+    function createTemplate(data) {
+        
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = 
+    `<!DOCTYPE html>
+       <head>
+    <title> ${title} </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+      </head>
 
-app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+            <body>
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                  <h3> 
+                      ${heading}
+                  </h3>
+                
+                   <div>
+                    ${date}
+                   </div>
+                
+                <div class='container'>
+                    
+                     <div>
+                       ${content}
+                     </div>
+                
+               </div> 
+            </body>
+            
+            </html>
+                
     
-});
-app.get('/article-two', function(req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
     
-});
-app.get('/article-three', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+    `
+    return htmlTemplate;
+}
+                app.get('/', function (req, res) {
+                res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+                });
+                
+                app.get('/article-one', function(req, res) {
+                res.send(createTemplate(articleOne));
+                
+                });
+                app.get('/article-two', function(req, res) {
+                res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+                
+                });
+                app.get('/article-three', function(req, res) {
+                res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+                
+                });
+                app.get('/ui/style.css', function (req, res) {
+                res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+                });
+                
+                app.get('/ui/madi.png', function (req, res) {
+                res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+                });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(`IMAD course app listening on port ${port}!`);
 });
