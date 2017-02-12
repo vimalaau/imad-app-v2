9dab5d646d467c135a77783ba2441c418b4c449d
vimalaau <vimya7@gmail.com>
2017-02-12 21:13:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,4 @@ console.log('Loaded!');
 // change the text of the id main-text
 
 var element = document.getElementById('main-text');
-element.innerHtml = 'New content';
+element.innerHTML = 'New content';
