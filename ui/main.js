3b6061da4c061168e776adb2314470162d8c27d2
vimalaau <vimya7@gmail.com>
2017-02-13 19:26:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = 'New content';
 
 // Move the image file
 var img = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 madi.onclick = function() {
-    madi.style.marginLeft = '300px';
+  var interval = setInterval(moveRight, 100);
 };
