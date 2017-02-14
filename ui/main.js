24d811fafc89a6680e21445976ad7c507701545c
vimalaau <vimya7@gmail.com>
2017-02-14 15:33:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,22 @@ var counter = 0;
 button.onclick = function() {
     
     // Make a request to the counter end
-    
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
-    
-    
-    //Render the variable to the correct span
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
+    request.onreadystateChange = function() {
+        if(request.readyState === XMLHttpRequest.Done) {
+            
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+
+//Make the request 
+request.open('GET' , 'http://vimalaau.imad.hasura-app.io/counter', true);
+request.send(null);
 };
