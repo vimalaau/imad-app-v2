67677b7b2e20160001bd1553e115f770c7511feb
vimalaau <vimya7@gmail.com>
2017-02-15 10:42:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function() {
     
     // Create a request object
@@ -12,7 +12,7 @@ button.onclick = function() {
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
     };
