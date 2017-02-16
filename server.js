d86f5b7b0d285bb39db47b77ebca8d08c2e73e83
vimalaau <vimya7@gmail.com>
2017-02-16 15:15:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,19 +81,7 @@ var content = data.content;
                 app.get('/', function (req, res) {
                 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
                 });
-                var counter = 0;
-                app.get('/counter', function(req, res){
-                    counter = counter + 1;
-                res.send(counter.toString()); 
-                });
                 
-                var names = [];
-                app.get('/submit-name/', function(req, res){ // we are getting as a query submit-name?xxxx
-                    var name = req.query.name;
-                    names.push(name);
-                    //JSON
-                    res.send(JSON.stringify(names));
-                });
                 
                 app.get('/:articleName', function(req, res) {
                 var articleName = req.params.articleName;
