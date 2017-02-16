b63df8abdc2c699f21f1119f66f09b420df035d2
vimalaau <vimya7@gmail.com>
2017-02-16 21:34:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,14 @@ var content = data.content;
                 });
                 
                 
-
+                app.get('submit-name/:name', function(req,res) {
+                    //get the name from the request
+                    var name = req.params.name;
+                    
+                    names.push(name);
+                    res.send(JSON.stringify(names));
+                    
+                });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
