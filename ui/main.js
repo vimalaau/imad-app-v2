ce46bc2076a9cc549e7c220d622de799fd3a243f
vimalaau <vimya7@gmail.com>
2017-02-16 21:07:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,21 @@ button.onclick = function() {
       request.send(null);
       
 };
+
+//submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
+    //make a request to the server and send the name
+    
+    //capture the list of the names and render it
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = "";
+    for (i=0;i<names.length;i++) {
+        list += '<li>' + names[i] + '</li>' ;
+        
+    }
+    var ul = document.getElementById("namelist");
+    ul.innerHTML = list;
+};
