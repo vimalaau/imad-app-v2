62414fca0f29905baf19614d1c9e5cec8de26126
vimalaau <vimya7@gmail.com>
2017-02-16 15:48:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,27 @@
 //code for counter endpoint
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
   
-  //Make a request to the counterpoint
-  
-  
-  // capture the response and store it in a variable
-  
-  // Render the variable in the correct span
-  
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+      //create a request to the counterpoint
+      var request = new XMLHttpRequest();
+      
+      
+      // capture the response and store it in a variable
+      request.onreadystatechange = function() {
+          if(request.readystate === XMLHttpRequest.DONE) {
+                //Take some action      
+                 if(request.status == 200) {
+                  var counter = request.responseText;   
+                  var span = document.getElementById('count');
+                  span.innerHTML = counter.toString();
+                 }
+          }
+          //else not
+      };
+      
+      //make a request
+      request.open('GET', 'http://vimalaau.imad.hasura-app.io/counter', true);
+      request.send(null);
+      
 };
