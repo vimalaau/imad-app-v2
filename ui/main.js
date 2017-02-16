d0abf2be4986b89fd32b9ace2b502f518eb5286f
vimalaau <vimya7@gmail.com>
2017-02-16 16:06:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //code for counter endpoint
-var button = document.getElementById('counter');
+var button = document.getElementById("counter");
 
 button.onclick = function() {
   
@@ -13,7 +13,7 @@ button.onclick = function() {
                 //Take some action      
                  if(request.status === 200) {
                   var counter = request.responseText;   
-                  var span = document.getElementById('count');
+                  var span = document.getElementById("count");
                   span.innerHTML = counter.toString();
                  }
           }
