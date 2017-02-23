11f7327bf38e66b039007e0334a5b6da884e9130
vimalaau <vimya7@gmail.com>
2017-02-23 12:27:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,7 @@ button.onclick = function() {
 };
 
 //submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
     //make a request to the server and send the name
@@ -57,6 +56,8 @@ submit.onclick = function() {
       };
       
       //make a request
+      var nameInput = document.getElementById("name");
+      var name = nameInput.value;
       request.open('GET', 'http://vimalaau.imad.hasura-app.io/submit-name?name=' + name, true);
       request.send(null);
     
