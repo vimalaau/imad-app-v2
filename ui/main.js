ee1b2f146d1a3a78a885fffb1e829a3fc11b3367
vimalaau <vimya7@gmail.com>
2017-02-23 12:14:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,12 +34,30 @@ submit.onclick = function() {
     //make a request to the server and send the name
     
     //capture the list of the names and render it
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = "";
-    for (i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>' ;
-        
-    }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+      
+      
+      // capture the response and store it in a variable
+      request.onreadystatechange = function() {
+          if(request.readyState === XMLHttpRequest.DONE) {
+                //Take some action      
+                 if(request.status === 200) {
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = "";
+                    for (i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>' ;
+                    }
+                    var ul = document.getElementById("namelist");
+                    ul.innerHTML = list;
+                  
+                 }
+          }
+          //else not
+      };
+      
+      //make a request
+      request.open('GET', 'http://vimalaau.imad.hasura-app.io/submit-name?name=' + name, true);
+      request.send(null);
+    
 };
