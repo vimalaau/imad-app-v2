798e5dd96a84f6d605ffa6ae6d04a6ff0385c935
vimalaau <vimya7@gmail.com>
2017-03-12 21:00:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var content = data.content;
                         if(err) {
                             res.status(500).send(err.toString());
                         } else {
-                            res.send(JSON.stringify(result));
+                            res.send(JSON.stringify(result.rows));
                         }
                     });
                 });
