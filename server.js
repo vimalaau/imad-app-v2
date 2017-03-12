b03fa8ef5cb24beccddf8e50f505e809d394dccc
vimalaau <vimya7@gmail.com>
2017-03-12 20:11:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database : 'vimalaau',
     host :'db.imad.hasura-app.io',
     port : '5432',
-    password : process.env.DB_PASSWORD.db-vimalaau-22830
+    password : 'db-vimalaau-22830'
 };
 
 var articles = {
