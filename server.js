af50ea91acfd568fd9cd6f3203f56943f4fb9605
vimalaau <vimya7@gmail.com>
2017-03-12 20:57:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,14 +93,14 @@ var content = data.content;
                 
                 
                 var pool = new Pool(config);
-                app.get('/test/test-db', function(req,res) {
+                app.get('/test-db', function(req,res) {
                     //Make a select request
                     //Return the request with results
                     pool.query('SELECT * FROM test', function(err,result) {
                         if(err) {
                             res.status(500).send(err.toString());
                         } else {
-                            res.send(JSON.stringify(result));
+                            res.send(JSON.stringify(result.row));
                         }
                     });
                 });
