c9f2918765172f1736c82b485554040607f95235
vimalaau <vimya7@gmail.com>
2017-03-12 20:58:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var content = data.content;
                         if(err) {
                             res.status(500).send(err.toString());
                         } else {
-                            res.send(JSON.stringify(result.row));
+                            res.send(JSON.stringify(result));
                         }
                     });
                 });
