6e29377bd623ed2157dee12a737561a2d6570edf
vimalaau <vimya7@gmail.com>
2017-03-12 20:03:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+    user : 'vimalaau',
+    database : 'vimalaau',
+    host :'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 
 var articles = {
 'article-one' : {
@@ -80,6 +89,20 @@ var content = data.content;
 }
                 app.get('/', function (req, res) {
                 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+                });
+                
+                
+                var pool = new Pool(config);
+                app.get('/test-db', function(req,res) {
+                    //Make a select request
+                    //Return the request with results
+                    pool.query('SELECT * FROM test', function(err,req) {
+                        if(err) {
+                            res.status(500).send(err.toString());
+                        } else {
+                            res.send(JSON.stringify(result));
+                        }
+                    });
                 });
                 
                 var counter = 0;
