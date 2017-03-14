cecff882dc757baa23671a169e56acd4027219f0
vimalaau <vimya7@gmail.com>
2017-03-14 22:35:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ var content = data.content;
                 
                 app.get('/articles/:articleName', function(req, res) {
                     
-                pool.query("SELECT * FROM article WHERE title =' "+ req.params.articleName +"'" , function(err,result) {
+                pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'" , function(err,result) {
                    if(err) {
                        res.status(500).send(err.toString());
                    } else {
