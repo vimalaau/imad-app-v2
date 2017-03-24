10ff117466cc0e96c4b17939b7058c0c7d5d7199
vimalaau <vimya7@gmail.com>
2017-03-24 22:35:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
