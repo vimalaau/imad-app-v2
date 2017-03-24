36a5fe02d881d35c227717a5a10502b86a71b94f
vimalaau <vimya7@gmail.com>
2017-03-24 22:46:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var content = data.content;
                 
                 function hash (input,salt) {
                     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-                    return hashed.toString('hex');
+                    return ['pbkdf2', '10000', salt, hashed.toString('hex')].join('$');
                     
                 }
                 
